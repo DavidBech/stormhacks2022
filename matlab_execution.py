fc401c7ed2cada7fb39e2b19c0748fd5e2f388ec
David <dbechert@sfu.ca>
2022-02-18 21:43:28 -0800

    added base info to files

--- matlab_execution.py
+++ matlab_execution.py
@@ -1,4 +1,6 @@
+import matlab
 
-class matlab_exection:
+class matlab_executioner:
     def __init__(self):
+        #self.matlabEngine = matlab.engine.start_matlab()
         pass
--- visualizer.py
+++ visualizer.py
@@ -1,10 +1,9 @@
-from matlab_execution import matlab_exection
-
-
+import figure_handle
 import matlab_execution
-import figure_handler
 
 if __name__ == "__main__":
+    figure_h = figure_handle.figure_handler()
+    matlab_e = matlab_execution.matlab_executioner()
     print("test")
     pass
 
