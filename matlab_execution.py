c35783ed8501a7463d71837930ded20328760d18
David <dbechert@sfu.ca>
2022-02-18 22:02:29 -0800

    added call to matlab engine

--- matlab_execution.py
+++ matlab_execution.py
@@ -1,6 +1,7 @@
 import matlab
+import matlab.engine
 
 class matlab_executioner:
     def __init__(self):
-        #self.matlabEngine = matlab.engine.start_matlab()
+        self.matlabEngine = matlab.engine.start_matlab()
         pass
