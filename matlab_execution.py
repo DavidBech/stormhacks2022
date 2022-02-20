439e6ccd0fae6d62fef18db1b78c8405c21fd96f
David <dbechert@sfu.ca>
2022-02-19 19:21:25 -0800

    julia set implementation

--- matlab_execution.py
+++ matlab_execution.py
@@ -13,7 +13,7 @@ Class For Handling matlab calls
 class matlab_executioner:
     def __init__(self, retValQueue):
         self.matlabEngine = matlab.engine.start_matlab()
-        self.matlabEngine.cd("../matlabScripts")
+        self.matlabEngine.cd("..\matlabScripts")
         self.requests = queue.Queue(maxsize=10)
         self.finished = queue.Queue()
         self.retValQueue = retValQueue
@@ -82,3 +82,17 @@ class matlab_executioner:
                     return
                 time.sleep(worker.loopSleepTime)
 
+    def callJulia(self, worker, request):
+        retVal = worker.matlabEngine.julia(request.args[0], request.args[1], request.args[2], request.args[3], nargout=0)
+        if worker.exit.is_set():
+            return
+        self.retValQueue.put((request,retVal))
+        finished = (worker)
+        while True:
+            try:
+                self.finished.put(finished, block=True, timeout=1)
+                return
+            except queue.Full:
+                if worker.exit.is_set():
+                    return
+                time.sleep(worker.loopSleepTime)
